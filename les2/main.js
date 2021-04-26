086bff524ac738c187da773f0e1acf099ce3d512
ВАШЕ_ИМЯAxelSFox <nimwenbadmod@gmail.com>
2021-04-26 13:17:17 +0300

    +

--- les2/main.js
+++ les2/main.js
@@ -2,90 +2,66 @@
 //Урок 2. ООП в JavaScript
 //1. Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.
 
-//const products = [
-//    {id: 1, title: 'Notebook', price: 20000},
-//    {id: 2, title: 'Mouse', price: 1500},
-//    {id: 3, title: 'Keyboard', price: 5000},
-//    {id: 4, title: 'Gamepad', price: 4500},
-//];
-//
-//const renderProduct = (title, price) => {
-//    return `<div class="product-item">
-//                <h3>${title}</h3>
-//               <p>${price}</p>
-//                <button class="by-btn">Добавить в корзину</button>
-//              </div>`;
-//}
-//const renderProducts = list => {
-//    document.querySelector('.products').innerHTML = list.map((item) => renderProduct(item.title, item.price)).join('');
-//}
-//renderProducts(products);
+//Класс для эелмента корзины. Формирует блок с id, названием и ценой товара 
 
-class ProductList {
-    #goods;
-    #allProducts;
-    #prop;
-
-    constructor(container = '.products') {
-        this.container = container;
-        this.#goods = [];
-        this.#allProducts = [];
-
-        this.#fetchGoods();
-        this.#render();
-    }
-
-    get property() {
-        return this.#prop;
-    }
-
-    set property(value) {
-        this.#prop = value;
-    }
-
-    #fetchGoods() {
-        this.#goods = [
-            {id: 1, title: 'Notebook', price: 20000},
-            {id: 2, title: 'Mouse', price: 1500},
-            {id: 3, title: 'Keyboard', price: 5000},
-            {id: 4, title: 'Gamepad', price: 4500},
-        ];
-    }
-
-    #render() {
-        const block = document.querySelector(this.container);
-
-        for (const good of this.#goods) {
-            const productObject = new ProductItem(good);
-            // console.log(productObject);
-            this.#allProducts.push(productObject);
-            block.insertAdjacentHTML('afterbegin', productObject.render());
-        }
-    }
-}
-
-class ProductItem {
-    constructor(product, img = 'https://via.placeholder.com/200x150') {
+class CartItem {
+        constructor(product) {
+        this.id = product.id;
         this.title = product.title;
         this.price = product.price;
-        this.id = product.id;
-        this.img = img;
+        this.render();
     }
-
+    
     render() {
         return `<div class="product-item" data-id="${this.id}">
-                      <img src="${this.img}" alt="Some img">
-                      <div class="desc">
-                          <h3>${this.title}</h3>
-                          <p>${this.price} \u20bd</p>
-                          <button class="buy-btn">Купить</button>
-                      </div>
-                  </div>`;
+            <div class="desc">
+            <h3>${this.title}</h3>
+            <p>${this.price} \u20bd</p>
+            <button class="del-btn">Удалить</button>
+        </div>
+    </div>`;
     }
-}
+            
+};
+
+//пустой класс корзины
+class Cart {
+    constructor(container ='.btn-cart', good){
+        this.container = container;
+        this.goods = good;
+        this.render();
+        this.sum();
+
+    };
 
 
-/*2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.*/
+    sum() {
+        return this.goods.reduce((sum, {price}) => sum + price, 0)
+    };
+
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (const good of this.goods) {
+            const productObject = new CartItem(good);
+            block.insertAdjacentHTML('afterbegin', productObject.render());
+        }
+
+};
+};
+
+let goods = [
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500},
+    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 4, title: 'Gamepad', price: 4500},
+    ];
+
+new Cart ('.btn-cart', goods);
+
+/*2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров.
+Такого классау нас нет и в материалах урока я его не нашла. Вероятно, имелся в виду ProductList. 
+Мне кажется, суммировать цену продуктов нужнее всего в рамках класса корзины*/
 
 
 
@@ -98,7 +74,7 @@ class ProductItem {
     ### С картофелем (+15 рублей, +10 калорий). ### Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) 
     и полить майонезом (+20 рублей, +5 калорий). ### 3Напишите программу, рассчитывающую стоимость и калорийность гамбургера. 
     Можно использовать примерную архитектуру класса из методички, но можно использовать и свою.*/
-class Hamburger {
+/*class Hamburger {
     constructor(size, stuffing) { ... }
     addTopping(topping) {    // Добавить добавку }
     removeTopping(topping) { // Убрать добавку }
@@ -107,7 +83,7 @@ class Hamburger {
     getStuffing() {          // Узнать начинку гамбургера }
     calculatePrice() {       // Узнать цену }
     calculateCalories() {    // Узнать калорийность }
-  }
+  }*/
 
 
 
